8292e0cf0c56f1a9ff5781b11ce83b585c39b093
Franklin Qu <qufrank0513@gmail.com>
2024-06-22 01:58:25 -0400

    create moviecard component

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,12 @@
+export const MovieCard = ({ movie, onClick }) => {
+    return (
+        <div
+            onClick={() => {
+                onClick(movie);
+            }}
+        >
+            {movie.title}
+        </div>
+    );
+ };
+ 
