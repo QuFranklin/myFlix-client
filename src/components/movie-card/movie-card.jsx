d7fe4534397fcdb3a8bc4adf36fe708d51d017fe
Franklin Qu <qufrank0513@gmail.com>
2024-06-28 13:03:19 -0400

    defined props constraints for MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onClick }) => {
     return (
         <div
@@ -7,6 +9,24 @@ export const MovieCard = ({ movie, onClick }) => {
         >
             {movie.title}
         </div>
-    );
- };
+    )
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        imagePath: PropTypes.string.isRequired,
+        genre: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired
+        }),
+        director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bio: PropTypes.string.isRequired,
+            birth_year: PropTypes.string
+        })
+    }).isRequired,
+    onClick: PropTypes.func.isRequired
+};
  
