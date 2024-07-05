54e6e49aec18a2f369c666acda13338a23c9793d
Franklin Qu <qufrank0513@gmail.com>
2024-07-05 18:36:12 -0400

    sign up button

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -71,7 +71,7 @@ export const SignupView = () => {
                 required
               />
           </label>
-          <button type="submit">Submit</button>
+          <button type="submit">Sign Up</button>
         </form>
     );
 };
