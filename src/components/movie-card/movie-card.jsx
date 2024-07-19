d6668def308e9f187a5e0a1151bea57270b0978f
Franklin Qu <qufrank0513@gmail.com>
2024-07-18 20:20:50 -0400

    implemented card and button components from React Bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,19 @@
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+
+
 export const MovieCard = ({ movie, onClick }) => {
     return (
-        <div
-            onClick={() => {
-                onClick(movie);
-            }}
-        >
-            {movie.title}
-        </div>
+        <Card className="h-100">
+        <Card.Img variant="top" src={movie.imagePath} />
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{movie.director.name}</Card.Text>
+                <Button onClick={() => onClick(movie)} variant="primary">
+                    View Movie
+                </Button>
+            </Card.Body>
+        </Card>
     );
- };
+  };
  
