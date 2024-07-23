a0f19d23ebe67ce8fce9c6ea5d34750a8199c286
Franklin Qu <qufrank0513@gmail.com>
2024-07-23 03:55:43 -0400

    console log to see user when sign in

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -2,6 +2,8 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
+  console.log("User:", user); // Check to see user
+  
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -25,7 +27,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
               <>
                 <Nav.Link as={Link} to="/">
                   Home
-                </Nav.Link>
+                </Nav.Link >
                 <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
               </>
             )}
