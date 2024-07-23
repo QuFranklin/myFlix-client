2d3c6f1647d333e32936452fd5a35884a662878e
Franklin Qu <qufrank0513@gmail.com>
2024-07-23 03:45:37 -0400

    added navigation

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,17 +4,25 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
+
 export const MainView = () => {
-    const [user, setUser] = useState([]);
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(storedUser? storedUser : null);
+    const [token, setToken] = useState(storedToken? storedToken : null);
     const [movies, setMovies] = useState([]);
+    
 
     useEffect(() => {
-        
+        if(!token) {
+            return;
+        }
     
         fetch("https://moviesdb-6abb3284c2fb.herokuapp.com/movies", {
             headers: { Authorization: 'Bearer ${token}' }
@@ -35,10 +43,11 @@ export const MainView = () => {
             }).catch((e) => {
                 console.log(e);
             });
-    }, []);
+    }, [token]);
     
         return (
             <BrowserRouter>
+            <NavigationBar user={user} onLoggedOut={() => { setUser(null); setToken(null); localStorage.clear()}} />
                 <Row className="justify-content-md-center">
                     <Routes>
                         <Route
@@ -50,7 +59,13 @@ export const MainView = () => {
                                     ) : (
                                         <Col md={5}>
                                             <LoginView 
-                                            onLoggedIn={(user) => setUser(user)} />
+                                                onLoggedIn={(user, token) => {
+                                                    setUser(user);
+                                                    setToken(token);
+                                                    localStorage.setItem("user", JSON.stringify(user));
+                                                    localStorage.setItem("token", token);
+                                                    }} 
+                                            />
                                         </Col>
                                     )}
                                 </>
@@ -71,7 +86,7 @@ export const MainView = () => {
                                 }
                             />
                             <Route
-                                path="/movies/:MovieID"
+                                path="/movies/:movieId"
                                 element={
                                     <>
                                         {!user ? (
@@ -80,7 +95,10 @@ export const MainView = () => {
                                                 <Col>The list is empty</Col>
                                             ) : (    
                                                 <Col md={8}>
-                                                    <MovieView movies={movies} />
+                                                    <MovieView 
+                                                    movies={movies}
+                                                    user={user}
+                                                    token={token}  />
                                                 </Col>
                                         )}
                                     </>
@@ -97,8 +115,8 @@ export const MainView = () => {
                                             ) : (
                                                 <> 
                                                     {movies.map((movie) => (
-                                                        <Col className="mb-5" key={movie.id} md={3}>
-                                                            <MovieCard movie={movie}/>
+                                                        <Col className="mb-4" key={movie._id} md={3}>
+                                                            <MovieCard movie={movie} />
                                                         </Col>
                                                     ))}
                                                 </>
