00f8e46817830428ecfbcaa559b24e1539b888b6
Franklin Qu <qufrank0513@gmail.com>
2024-07-25 07:44:51 -0400

    path to profile that display user profile

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,6 +5,7 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
+import { ProfileView } from "../profile-view/profile-view";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -80,6 +81,20 @@ export const MainView = () => {
                                         ) : (
                                             <Col md={5}>
                                                 <SignupView />
+                                            </Col>
+                                        )}
+                                    </>
+                                }
+                            />
+                            <Route
+                                path="/profile"
+                                element={
+                                    <>
+                                        {!user ? (
+                                            <Navigate to="/login" replace />
+                                        ) : (
+                                            <Col md={5}>
+                                                <ProfileView />
                                             </Col>
                                         )}
                                     </>
