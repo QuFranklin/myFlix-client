1e7b06202da6eea1e97a0f286f6e85394d2256e8
Franklin Qu <qufrank0513@gmail.com>
2024-07-31 07:53:32 -0400

    added add to & remove from Favorite button

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,12 +1,65 @@
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 import './movie-view.scss';
 
-export const MovieView = ({ movies }) => {
+export const MovieView = ({ movies, user, token, setUser }) => {
     const { movieId } = useParams();
+    const [isFavorite, setIsFavorite] = useState(false);
     const movie = movies.find((b) => b.id === movieId);
-    
+
+    useEffect(() => {
+        if(user && user.FavoriteMovies)  {
+            const isFavorite = user.FavoriteMovies.includes(movieId);
+            setIsFavorite(isFavorite);
+        }
+    }, [movieId, user]);
+
+    const addtoFavorite = () => {
+        fetch(`https://moviesdb-6abb3284c2fb.herokuapp.com/users/${user.Username}/${movieId}`,
+        {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}` 
+            }
+        }).then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+        })
+        .then((data) => {
+            setUser(data);
+            localStorage.setItem("user", JSON.stringify(data));
+            setIsFavorite(true);
+        })
+        .catch((e) => {
+            console.log(e);
+        });       
+    };
+    const removefromFavorite = () => {
+        fetch(`https://moviesdb-6abb3284c2fb.herokuapp.com/users/${user.Username}/${movieId}`,
+        {
+            method: "DELETE",
+            headers: { 
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}` 
+            }
+        }).then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+        })
+        .then((data) => {
+            setUser(data);
+            localStorage.setItem("user", JSON.stringify(data));
+            setIsFavorite(false);
+        })
+        .catch((e) => {
+        console.log(e);
+        });       
+    };
     return (
         <div>
             <div>
@@ -33,6 +86,14 @@ export const MovieView = ({ movies }) => {
             <Link to={`/`}>
                 <button className="back-button">Back</button>
             </Link>
+            <div>
+                {isFavorite ? (
+                    <Button onClick={removefromFavorite}>Remove from favorite</Button>
+                ) : (
+                    <Button onClick={addtoFavorite}>Add to favorite</Button>   
+                )}
+            
+            </div>
         </div>
     );
 };
